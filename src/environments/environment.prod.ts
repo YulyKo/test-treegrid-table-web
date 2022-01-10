203ad6b4d2a0d6d929b2da128889793911915156
YulyKo <yuliakotenko1.0@gmail.com>
2022-01-10 16:14:20 +0200

    changed ws -> wss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_URL: 'https://treegrid-task-api.herokuapp.com/api/',
-  SOCKET_URL: 'ws://treegrid-task-api.herokuapp.com'
+  SOCKET_URL: 'wss://treegrid-task-api.herokuapp.com'
 };
